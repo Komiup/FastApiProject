4437def66a682dce66563dffc7d88d055f4f1a1c
Komiup <komiup@gmail.com>
2025-03-14 15:29:12 +0300

    fixed router

--- src/api/routers/base_router.py
+++ src/api/routers/base_router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-
+from src.api.schemas.schemas_1 import ResponseBase
 from src.api.services.service_1 import responce_base_router
 
 router = APIRouter()
 
 
-@router.get("/", response_model=BaseModel)
+@router.get("/", response_model=ResponseBase)
 async def read_root():
     result = await responce_base_router()
     return result
